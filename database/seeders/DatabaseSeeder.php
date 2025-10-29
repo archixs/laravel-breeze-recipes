9af2c91a257f942ab2b54d37077f07cbbb8f7b3b
Arturs <arturshugo4@gmail.com>
2025-10-29 14:10:08 +0200

    Added missing line for RecipeSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             RecipeCategorySeeder::class,
+            RecipeSeeder::class
         ]);
 
         User::factory()->create([
