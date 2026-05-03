a695732773e9a2cb0a70a65aca6704874aaddf2e
Arturs <arturshugo4@gmail.com>
2026-05-03 17:31:45 +0300

    Add split screen image and favicon to auth layout

--- resources/views/components/auth-layout.blade.php
+++ resources/views/components/auth-layout.blade.php
@@ -5,12 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="min-h-screen bg-gray-100 flex">
 
-    <div class="hidden md:flex w-1/2 items-center justify-center bg-gray-100">
-        <x-application-logo class="w-40 h-auto text-indigo-600" />
+    <div class="hidden md:flex w-1/2 bg-cover bg-center relative" 
+        style="background-image: url('{{ asset('storage/demo/login-bg.jpg') }}' );">
+        
+        <div class="absolute inset-0 bg-black/70"></div>
+
+        <div class="relative z-10 flex items-center justify-center w-full">
+            <div class="bg-white/90 p-8 rounded-full shadow-2xl">
+                <img src="{{ asset('storage/demo/recipes-icon2.jpg') }}" alt="Logo" class="w-40 h-auto">
+            </div>
+        </div>
     </div>
 
     <!-- Right side (content / form) -->
