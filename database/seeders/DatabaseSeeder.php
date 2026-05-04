752e66ea8c8dca013e362f192592d9c47f93483e
Arturs <arturshugo4@gmail.com>
2026-05-05 00:40:45 +0300

    Register RecipeSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             RecipeCategorySeeder::class,
-            RecipeSeeder::class
+            RecipeSeeder::class,
+            RatingSeeder::class
         ]);
 
         User::factory()->create([
