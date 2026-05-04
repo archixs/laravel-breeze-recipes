7af8f236509b4c3668bca264391e3d5fb35895cf
Arturs <arturshugo4@gmail.com>
2026-05-05 00:42:32 +0300

    Declutter web.php file

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,6 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/dashboard', function () {
     return redirect()->route('index');
 })->name('dashboard');
@@ -19,7 +15,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    // Route::get('/recipe', [RecipeController::class, 'index'])->name('index');
     Route::get('/recipe/create', [RecipeController::class, 'create'])->name('create');
     Route::get('/recipe/{id}/edit', [RecipeController::class, 'edit'])->name('edit');
     Route::post('/', [RecipeController::class, 'save']);
